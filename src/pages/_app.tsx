71a692edd3a46ebf8abec19f40069a788a10a7d4
Rafhael <rafhael.dev@gmail.com>
2025-02-06 09:55:27 -0300

    feat: component header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import type { AppProps } from "next/app";
+import { AppProps } from 'next/app';
+import { Header } from '../components/Header';
 import '../styles/global.scss';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
